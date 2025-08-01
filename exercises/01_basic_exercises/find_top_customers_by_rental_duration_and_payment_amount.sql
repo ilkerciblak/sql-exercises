2f3cb9e017fc0d38f64bf5a41c20b2987941a365
ilkerciblak <ilkerciblak@gmail.com>
2025-08-01 09:54:49 +0300

    chore(exercise): add new exercise

--- exercises/01_basic_exercises/find_top_customers_by_rental_duration_and_payment_amount.sql
+++ exercises/01_basic_exercises/find_top_customers_by_rental_duration_and_payment_amount.sql
@@ -5,7 +5,7 @@
 -- # Results must include customers' id, first name and last name, total rental duration and total payment amount
 -- # Sort the results by total payment amounts in descending order.
 
-SELECT
+ SELECT
     customer.customer_id,
     customer.first_name,
     customer.last_name,
@@ -13,7 +13,7 @@ SELECT
     SUM(payment.amount) AS total_payment
 FROM customer
 INNER JOIN payment ON payment.customer_id = customer.customer_id
-INNER JOIN rental ON rental.customer_id = customer.customer_id
+INNER JOIN rental ON rental.rental_id = payment.rental_id
 GROUP BY 
     customer.customer_id,
     customer.first_name,
@@ -21,5 +21,32 @@ GROUP BY
 HAVING 
     ROUND(SUM(EXTRACT(EPOCH FROM (rental.return_date - rental.rental_date)) / 60)) > 1000
     AND SUM(payment.amount) > 200
-ORDER BY total_payment DESC;
+ORDER BY total_payment DESC; 
+
+-- USING CTE
+
+WITH rental_cte AS (
+    SELECT  
+        customer.customer_id as customer_id,
+        customer.first_name as first_name,
+        customer.last_name as last_name,
+        ROUND(SUM(EXTRACT(EPOCH FROM (rental.return_date - rental.rental_date)) / 60)) as rental_duration_min,
+        SUM(payment.amount) AS total_payment
+    FROM rental
+    INNER JOIN customer ON customer.customer_id = rental.customer_id
+    INNER JOIN payment ON payment.rental_id = rental.rental_id
+    GROUP BY 
+        customer.customer_id,
+        customer.first_name, 
+        customer.last_name 
+    ORDER BY total_payment DESC
+)
+SELECT 
+    customer_id,
+    first_name,
+    last_name,
+    rental_duration_min,
+    total_payment
+FROM rental_cte
+WHERE total_payment > 200 AND rental_duration_min > 1000;
 
